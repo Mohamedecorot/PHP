bd210903681739a37b7b0057d6a4ea4a2de5a668
guerroui.m@gmail.com <guerroui.m@gmail.com>
2020-11-18 16:32:13 +0100

    TP: dashboard: ajout du nbr detaillé des vues par mois et par jour

--- 08PHP&HTML/dashboard.php
+++ 08PHP&HTML/dashboard.php
@@ -1,9 +1,14 @@
 <?php
 require_once 'functions/compteur.php';
-$total = nombre_vues();
 $annee = (int)date('Y');
 $annee_selection = empty($_GET['annee']) ? null : (int)$_GET['annee'];
 $mois_selection = empty($_GET['mois']) ? null : $_GET['mois'];
+if ($annee_selection && $mois_selection) {
+    $total = nombre_vues_mois($annee_selection, $mois_selection);
+    $detail = nombre_vues_detail_mois($annee_selection, $mois_selection);
+} else {
+    $total = nombre_vues();
+}
 
 $mois = [
     '01' => 'Janvier',
@@ -40,12 +45,31 @@ require 'elements/header.php';
         </div>
     </div>
     <div class="col-md-8">
-        <div class="card">
+        <div class="card mb-4">
             <div class="card-body">
                 <strong style="font-size:3em"><?= $total ?></strong><br>
                 Visite<?= $total > 1 ? 's': '' ?> total
             </div>
         </div>
+        <?php if (isset($detail)): ?>
+            <h2>Détails des visites pour le mois</h2>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Jour</th>
+                        <th>Nombre de visite</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach($detail as $ligne): ?>
+                    <tr>
+                        <td><?= $ligne['jour'] ?></td>
+                        <td><?= $ligne['visites'] ?> visite<?= $ligne['visites'] > 1 ? 's' : '' ?></td>
+                    </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        <?php endif; ?>
     </div>
 </div>
 
