165f9cfda2cce6ac354e06717b458a0340dd6aa2
guerroui.m@gmail.com <guerroui.m@gmail.com>
2020-11-20 16:06:44 +0100

    TP: Livre d'or - formulaire, traitement du formulaire invalide

---
+++ 27Livre_dor/index.php
@@ -0,0 +1,44 @@
+<?php
+require_once 'classes/Message.php';
+//require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'class' . DIRECTORY_SEPARATOR . 'Message.php';
+$errors = null;
+if (isset($_POST['username'], $_POST['message'])) {
+    $message = new Message($_POST['username'], $_POST['message']);
+    if ($message->isValid()) {
+
+    } else {
+        $errors = $message->getError();
+    }
+}
+
+$title = "Livre d'or";
+require 'elements/header.php';
+?>
+
+<div class="container">
+    <h1>Livre d'or</h1>
+
+    <?php if (!empty($errors)): ?>
+        <div class="alert alert-danger">
+            Formulaire invalide
+        </div>
+    <?php endif ?>
+
+    <form action="" method="post">
+        <div class="form-group">
+            <input value="<?= htmlentities($_POST['username'] ?? '')?>" type="text" name="username" placeholder="Votre pseudo" class="form-control <?= isset($errors['username']) ? 'is-invalid': '' ?>">
+            <?php if (isset($errors['username'])): ?>
+                <div class="invalid-feedback"><?= $errors['username'] ?></div>
+            <?php endif ?>
+        </div>
+        <div class="form-group">
+            <textarea name="message" placeholder="Votre message" class="form-control <?= isset($errors['message']) ? 'is-invalid': '' ?>"><?= htmlentities($_POST['message'] ?? '')?></textarea>
+            <?php if (isset($errors['message'])): ?>
+                <div class="invalid-feedback"><?= $errors['message'] ?></div>
+            <?php endif ?>
+        </div>
+        <button class="btn btn-primary">Envoyer</button>
+    </form>
+</div>
+
+<?php require 'elements/footer.php'; ?>
