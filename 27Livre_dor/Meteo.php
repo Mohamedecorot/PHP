ed6d7678831a895ec2507334cefd6b5398fbf1aa
guerroui.m@gmail.com <guerroui.m@gmail.com>
2020-11-23 13:46:25 +0100

    Les exceptions: Traitement des erreurs php de base (class Error)

--- 27Livre_dor/Meteo.php
+++ 27Livre_dor/Meteo.php
@@ -1,12 +1,14 @@
 <?php
+declare(strict_types=1);
 require_once 'classes/OpenWeather.php';
 $weather = new OpenWeather('94c6cf0868fa5cb930a5e2d71baf0dbf');
 $error = null;
 try {
+    //$data = explode(' ');
     $forecast = $weather->getForecast('Marseille,fr');
     $today = $weather->getToday('Marseille,fr');
-} catch (CurlException $e) {
-    exit($e->getMessage());
+} catch (CurlException | Error $e) {
+    $error = $e->getMessage();
 } catch (HTTPException $e) {
     $error = $e->getCode() . ' : ' . $e->getMessage();
 }
