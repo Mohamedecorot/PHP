14dcc235dbe9a9c7349f7a43a52daca722958436
guerroui.m@gmail.com <guerroui.m@gmail.com>
2020-11-24 11:01:46 +0100

    PDO et bdd: utilisation de class pour representer les données, PDO::FETCH_CLASS

--- 27Livre_dor/blog/index.php
+++ 27Livre_dor/blog/index.php
@@ -1,4 +1,5 @@
 <?php
+require_once '../classes/Post.php';
 $pdo = new PDO('sqlite:../data.db', null, null, [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
@@ -16,7 +17,8 @@ try {
         header('Location: /blog/edit.php?id=' . $pdo->lastInsertId());
     }
     $query = $pdo->query('SELECT * FROM posts');
-    $posts =$query->fetchAll();
+    /** @var Post[] */
+    $posts =$query->fetchAll(PDO::FETCH_CLASS, 'Post');
 } catch (PDOException $e) {
     $error = $e->getMessage();
 }
@@ -34,7 +36,11 @@ require '../elements/header.php';
     <?php else: ?>
         <ul>
             <?php foreach($posts as $post): ?>
-                <li><a href="/blog/edit.php?id=<?= $post->id ?>"><?= htmlentities($post->name) ?></a></li>
+                <h3><a href="/blog/edit.php?id=<?= $post->id ?>"><?= htmlentities($post->name) ?></a></h3>
+                <p class="small text-muted">Ecrit le <?= $post->created_at->format('d/m/Y à H:i') ?>)</p>
+                <p>
+                    <?= nl2br(htmlentities($post->getExcerpt())) ?>
+                </p>
             <?php endforeach ?>
         </ul>
 
