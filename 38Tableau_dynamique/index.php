cf688eedab67a6a4a33896a5727b1197497bcaf0
guerroui.m@gmail.com <guerroui.m@gmail.com>
2020-11-27 20:36:03 +0100

    Exercice: tableau_dynamique [affichage des données de la bdd sour forme de tableau]

--- 38Tableau_dynamique/index.php
+++ 38Tableau_dynamique/index.php
@@ -1,5 +1,16 @@
+<?php
+require 'vendor/autoload.php';
+$pdo = new PDO("sqlite:./products.db", null, null, [
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+]);
+$products = $pdo->query("SELECT * FROM products LIMIT 20")->fetchAll();
+//dd($products);
+
+?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,13 +21,23 @@
     <table class="table table-striped">
         <thead>
             <tr>
-                <th></th>
+                <th>ID</th>
+                <th>Nom</th>
+                <th>Prix</th>
+                <th>Ville</th>
+                <th>Adresse</th>
             </tr>
         </thead>
         <tbody>
+            <?php foreach($products as $product): ?>
             <tr>
-                <td></td>
+                <td><?= $product['id'] ?></td>
+                <td><?= $product['name'] ?></td>
+                <td><?= number_format($product['price'], 0, '', ' ') ?> €</td>
+                <td><?= $product['city'] ?></td>
+                <td><?= $product['address'] ?></td>
             </tr>
+            <?php endforeach ?>
         </tbody>
     </table>
 </body>
