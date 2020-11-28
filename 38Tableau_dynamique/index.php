c87649c561e17d6da9ba2ab863937a9cd665bfa4
guerroui.m@gmail.com <guerroui.m@gmail.com>
2020-11-28 14:14:14 +0100

    Exercice: tableau_dynamique [Ajout du formulaire de recherche]

--- 38Tableau_dynamique/index.php
+++ 38Tableau_dynamique/index.php
@@ -17,7 +17,17 @@ $products = $pdo->query("SELECT * FROM products LIMIT 20")->fetchAll();
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <title>Biens immobiliers</title>
 </head>
-<body>
+<body class="p-4">
+
+    <h1>Les biens immobiliers</h1>
+
+    <form action="" class="mb-4">
+        <div class="form-group">
+            <input type="text" class="form-control" name="q" placeholder="Rechercher par ville">
+        </div>
+        <button class="btn btn-primary">Rechercher</button>
+    </form>
+
     <table class="table table-striped">
         <thead>
             <tr>
@@ -33,7 +43,7 @@ $products = $pdo->query("SELECT * FROM products LIMIT 20")->fetchAll();
             <tr>
                 <td><?= $product['id'] ?></td>
                 <td><?= $product['name'] ?></td>
-                <td><?= number_format($product['price'], 0, '', ' ') ?> €</td>
+                <td><?= number_format($product['price'], 0, '', ' ') //NumberHelper::price($product['price'])?> €</td>
                 <td><?= $product['city'] ?></td>
                 <td><?= $product['address'] ?></td>
             </tr>
