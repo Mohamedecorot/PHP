077e1eca4ab2dbaa8c2452c01b67ac5fc51599d4
guerroui.m@gmail.com <guerroui.m@gmail.com>
2020-12-02 14:22:29 +0100

    QueryBuilder[ajout de la fonction from (et toSQL)]

--- 42QueryBuilder/src/QueryBuilder.php
+++ 42QueryBuilder/src/QueryBuilder.php
@@ -3,5 +3,16 @@ namespace App;
 
 class QueryBuilder {
 
+    private $from;
 
+    public function from (string $table, string $alias): self
+    {
+        $this->from ="$table $alias";
+        return $this;
+    }
+
+    public function toSQL(): string
+    {
+    return "SELECT * FROM {$this->from}";
+    }
 }
