5c7891a769f019f5470bc51f9651a372840d80b1
guerroui.m@gmail.com <guerroui.m@gmail.com>
2020-12-02 14:28:21 +0100

    QueryBuilder[ajout de la fonction OrderBy]

--- 42QueryBuilder/src/QueryBuilder.php
+++ 42QueryBuilder/src/QueryBuilder.php
@@ -5,14 +5,26 @@ class QueryBuilder {
 
     private $from;
 
+    private $order;
+
+
     public function from (string $table, string $alias): self
     {
         $this->from ="$table $alias";
         return $this;
     }
 
+    public function orderBy (string $key, string $direction): self
+    {
+        $this->order ="$key $direction";
+        return $this;
+    }
+
     public function toSQL(): string
     {
-    return "SELECT * FROM {$this->from}";
+        $sql = "SELECT * FROM {$this->from}";
+        if ($this->order) {
+            $sql .= " ORDER BY {$this->order}";
+        }
     }
 }
