cd8641d7e28cd7ef5d1d1d605f5a04ec2319c690
guerroui.m@gmail.com <guerroui.m@gmail.com>
2020-12-02 14:34:37 +0100

    QueryBuilder[testMultipleOrderBy]

--- 42QueryBuilder/src/QueryBuilder.php
+++ 42QueryBuilder/src/QueryBuilder.php
@@ -5,26 +5,31 @@ class QueryBuilder {
 
     private $from;
 
-    private $order;
+    private $order = [];
 
 
-    public function from (string $table, string $alias): self
+    public function from (string $table, string $alias = null): self
     {
-        $this->from ="$table $alias";
+        $this->from = $alias === null ? $table : "$table $alias";
         return $this;
     }
 
     public function orderBy (string $key, string $direction): self
     {
-        $this->order ="$key $direction";
+        $direction = strtoupper($direction);
+        if (!in_array($direction, ['ASC', 'DESC'])) {
+            $this->order[] = $key;
+        } else {
+            $this->order[] ="$key $direction";
+        }
         return $this;
     }
 
     public function toSQL(): string
     {
         $sql = "SELECT * FROM {$this->from}";
-        if ($this->order) {
-            $sql .= " ORDER BY {$this->order}";
+        if (!empty($this->order)) {
+            $sql .= " ORDER BY " . implode(', ', $this->order);
         }
     }
 }
