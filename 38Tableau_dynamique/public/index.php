46dd5753d4b6f105fa0c6d6581e8d64b4cca9c40
guerroui.m@gmail.com <guerroui.m@gmail.com>
2020-12-03 13:51:56 +0100

    Tableau dynamique[class Table fonctionnelle]

--- 38Tableau_dynamique/public/index.php
+++ 38Tableau_dynamique/public/index.php
@@ -14,7 +14,6 @@ $pdo = new PDO("sqlite:../products.db", null, null, [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 ]);
 $query = (new QueryBuilder($pdo))->from('products');
-$sortable = ["id", "name", "city", "price", "address"];
 
 //Recherche par ville
 if (!empty($_GET['q'])) {
@@ -24,48 +23,13 @@ if (!empty($_GET['q'])) {
 
 }
 
-$count = (clone $query)->count();
-
-//Organisation
-if (!empty($_GET['sort']) && in_array($_GET['sort'], $sortable)) {
-    $query->orderBy($_GET['sort'], $_GET['dir'] ?? 'asc');
-}
-
-
-//Pagination
-$query
-    ->limit(PER_PAGE)
-    ->page($_GET['p'] ?? 1);
-$products = $query->fetchAll();
 $table = new Table($query, $_GET);
-$table->sortable('id', 'ville');
-$table->setColumns([
+$table->sortable('id', 'city');
+$table->Columns([
     'id' => 'ID',
     'name' => 'Nom',
     'city' => 'Ville'
 ]);
-
-
-//pour connaitre le nombre de page
-$pages = ceil($count/ PER_PAGE);
-
-//pour trier les labels par ordre croissant ou descroissant
-function sortir(string $sortKey, string $label, array $data): string {
-    $sort = $data['sort'] ?? null;
-    $direction = $data['dir'] ?? null;
-    $icon= "";
-    if ($sort === $sortKey) {
-        $icon = $direction === 'asc' ? "^" : "v";
-    }
-    $url = http_build_query(array_merge($data, [
-        'sort' => $sortKey,
-        'dir' => $direction === 'asc' && $sort === $sortKey ? 'desc' : 'asc'
-        ]));
-        return <<<HTML
-        <a href="?$url">$label $icon</a>
-HTML;
-}
-
 
 ?>
 
@@ -89,38 +53,6 @@ HTML;
     </form>
 
     <?php $table->render() ?>
-
-    <table class="table table-striped">
-        <thead>
-            <tr>
-                <th><?= sortir('id', 'ID', $_GET) ?></th>
-                <th><?= sortir('name', 'Nom', $_GET) ?></th>
-                <th><?= sortir('price', 'Prix', $_GET) ?></th>
-                <th><?= sortir('city', 'Ville', $_GET) ?></th>
-                <th><?= sortir('address', 'Adresse', $_GET) ?></th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach($products as $product): ?>
-            <tr>
-                <td>#<?= $product['id'] ?></td>
-                <td><?= $product['name'] ?></td>
-                <td><?= number_format($product['price'], 0, '', ' ') //NumberHelper::price($product['price'])?> €</td>
-                <td><?= $product['city'] ?></td>
-                <td><?= $product['address'] ?></td>
-            </tr>
-            <?php endforeach ?>
-        </tbody>
-    </table>
-
-
-<?php if ($pages > 1 && $page > 1): ?>
-    <a href="?<?= http_build_query(array_merge($_GET, ["p" => $page - 1])) ?>" class="btn btn-primary">Page précédente</a>
-<?php endif ?>
-
-<?php if ($pages > 1 && $page < $pages): ?>
-    <a href="?<?= http_build_query(array_merge($_GET, ["p" => $page + 1])) ?>" class="btn btn-primary">Page suivante</a>
-<?php endif ?>
 
 </body>
 </html>
--- 38Tableau_dynamique/src/Table.php
+++ 38Tableau_dynamique/src/Table.php
@@ -3,13 +3,15 @@ namespace App;
 
 class Table {
 
-    private $uery;
+    private $query;
 
     private $get;
 
     private $sortable = [];
 
     private $columns = [];
+
+    private $limit = 20;
 
     public function __construct(QueryBuilder $query, array $get)
     {
@@ -23,5 +25,73 @@ class Table {
 
     public function columns(array $columns) {
         $this->columns = $columns;
+    }
+
+    //pour trier les labels par ordre croissant ou descroissant
+    public function th(string $sortKey): string {
+        if (!in_array($sortKey, $this->sortable)) {
+            return $this->columns[$sortKey];
+        }
+        $sort = $this->get['sort'] ?? null;
+        $direction = $this->get['dir'] ?? null;
+        $icon= "";
+        if ($sort === $sortKey) {
+            $icon = $direction === 'asc' ? "^" : "v";
+        }
+        $url = http_build_query(array_merge($this->get, [
+            'sort' => $sortKey,
+            'dir' => $direction === 'asc' && $sort === $sortKey ? 'desc' : 'asc'
+            ]));
+            return <<<HTML
+            <a href="?$url">{$this->columns[$sortKey]} $icon</a>
+HTML;
+    }
+
+    public function render()
+    {
+        $page = $this->get['p'] ?? 1;
+        $count = (clone $this->query)->count();
+        //organisation
+        if (!empty($this->get['sort']) && in_array($this->get['sort'], $this->sortable)) {
+            $this->query->orderBy($this->get['sort'], $this->get['dir'] ?? 'asc');
+        }
+        //Pagination
+        $items = $this->query
+            ->select(array_keys($this->columns))
+            ->limit($this->limit)
+            ->page($page)
+            ->fetchAll();
+
+        //pour connaitre le nombre de page
+        $pages = ceil($count/ $this->limit);
+        ?>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <?php foreach($this->columns as $key => $columns): ?>
+                        <th><?= $this->th($key) ?></th>
+                    <?php endforeach ?>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($items as $item): ?>
+                <tr>
+                    <?php foreach($this->columns as $key => $columns): ?>
+                        <td><?= $item[$key] ?></td>
+                    <?php endforeach ?>
+                </tr>
+                <?php endforeach ?>
+            </tbody>
+        </table>
+
+        <?php if ($pages > 1 && $page > 1): ?>
+            <a href="?<?= http_build_query(array_merge($this->get, ["p" => $page - 1])) ?>" class="btn btn-primary">Page précédente</a>
+        <?php endif ?>
+
+        <?php if ($pages > 1 && $page < $pages): ?>
+            <a href="?<?= http_build_query(array_merge($this->get, ["p" => $page + 1])) ?>" class="btn btn-primary">Page suivante</a>
+        <?php endif ?>
+
+    <?php
     }
 }
