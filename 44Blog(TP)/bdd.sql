565c019f6c0eeb0970bcbb1347b83b3be9e64929
guerroui.m@gmail.com <guerroui.m@gmail.com>
2020-12-04 16:34:39 +0100

    TP_Blog_PHP[création de la structure: ajout de la table post]

---
+++ 44Blog(TP)/bdd.sql
@@ -0,0 +1,9 @@
+CREATE TABLE post (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    slug VARCHAR(255) NOT NULL,
+    content TEXT(650000) NOT NULL,
+    created_at DATETIME NOT NULL,
+    PRIMARY KEY (id)
+)
+
