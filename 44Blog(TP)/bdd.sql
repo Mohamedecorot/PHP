1adf6170ef3b1083956ff6bb8f91b2c3d5af016f
guerroui.m@gmail.com <guerroui.m@gmail.com>
2020-12-04 16:35:45 +0100

    TP_Blog_PHP[création de la structure: ajout de la table post_category]

--- 44Blog(TP)/bdd.sql
+++ 44Blog(TP)/bdd.sql
@@ -13,3 +13,20 @@ CREATE TABLE category (
     slug VARCHAR(255) NOT NULL,
     PRIMARY KEY (id)
 )
+
+CREATE TABLE post_category (
+    post_id INT UNSIGNED NOT NULL,
+    category_id INT UNSIGNED NOT NULL,
+    PRIMARY KEY (post_id, category_id),
+    CONSTRAINT fk_post
+        FOREIGN KEY (post_id)
+        REFERENCES post (id)
+        ON DELETE CASCADE
+        ON UPDATE RESTRICT,
+    CONSTRAINT fk_category
+        FOREIGN KEY (category_id)
+        REFERENCES category (id)
+        ON DELETE CASCADE
+        ON UPDATE RESTRICT
+)
+
