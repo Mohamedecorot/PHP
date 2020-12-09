6e8755092b6073e9a9e601392e0fd5dde396ce66
guerroui.m@gmail.com <guerroui.m@gmail.com>
2020-12-09 21:52:27 +0100

    TP_Blog_PHP [page article: 'amélioration du code de la page index en utilsant la class paginatedQuery']

--- 44Blog(TP)/views/post/index.php
+++ 44Blog(TP)/views/post/index.php
@@ -3,22 +3,18 @@ use App\URL;
 use App\Connection;
 use App\Model\Post;
 use App\Helpers\Text;
+use App\PaginatedQuery;
 
 $title = 'Mon blog';
 $pdo = Connection::getPDO();
 
-$currentPage = URL::getPositiveInt('page', 1);
-
-//nombre d'article:
-$count = (int)$pdo->query('SELECT COUNT(id) FROM post')->fetch(PDO::FETCH_NUM)[0];
-$perPage = 12;
-$pages = ceil($count / $perPage);
-if($currentPage > $pages) {
-    throw new Exception('cette page n\'existe pas');
-}
-$offset = $perPage * ($currentPage - 1);
-$query = $pdo->query("SELECT * FROM post ORDER BY created_at DESC LIMIT $perPage OFFSET $offset");
-$posts = $query->fetchAll(PDO::FETCH_CLASS, Post::class);
+$paginatedQuery = new PaginatedQuery(
+    "SELECT * FROM post ORDER BY created_at DESC",
+    "SELECT COUNT(id) FROM post",
+    Post::class
+);
+$posts = $paginatedQuery->getItems();
+$link = $router->url('home');
 
 ?>
 
@@ -33,14 +29,6 @@ $posts = $query->fetchAll(PDO::FETCH_CLASS, Post::class);
 </div>
 
 <div class="d-flex justify-content-between my-4">
-    <?php if ($currentPage > 1): ?>
-        <?php
-        $link = $router->url('home');
-        if($currentPage > 2) $link .= '?page=' . ($currentPage - 1);
-        ?>
-        <a href="<?= $link ?>" class="btn btn-primary">&laquo; Page précédente</a>
-    <?php endif ?>
-    <?php if ($currentPage < $pages): ?>
-        <a href="<?= $router->url('home') ?>?page=<?= $currentPage + 1 ?>" class="btn btn-primary ml-auto">Page suivante &raquo;</a>
-    <?php endif ?>
+    <?= $paginatedQuery->previousLink($link) ?>
+    <?= $paginatedQuery->nextLink($link) ?>
 </div>
