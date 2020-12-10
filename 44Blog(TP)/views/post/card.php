190ee1d4a6ef04c49edf11870ad45886c1670e7b
guerroui.m@gmail.com <guerroui.m@gmail.com>
2020-12-10 16:38:29 +0100

    TP_Blog_PHP [Affichage des catégories sur le listing : 'affichage des catégories aux articles']

--- 44Blog(TP)/views/post/card.php
+++ 44Blog(TP)/views/post/card.php
@@ -1,7 +1,15 @@
 <div class="card mb-3">
     <div class="card-body">
         <h5 class="card-title"><?= htmlentities($post->getName()) ?></h5>
-        <p class="text-muted"><?= $post->getCreatedAt()->format('d F Y H:i') ?></p>
+        <p class="text-muted"><?= $post->getCreatedAt()->format('d F Y H:i') ?> ::
+<?php foreach($post->getCategories() as $k => $category):
+    if ($k > 0):
+        echo ',';
+    endif;
+    $caterogy_url = $router->url('category', ['id' => $category->getID(), 'slug' => $category->getSlug()]);
+    ?><a href="<?= $caterogy_url ?>"><?= e($category->getName()) ?></a><?php
+endforeach ?>
+    </p>
         <p><?= $post->getExcerpt() ?></p>
         <p>
             <a href="<?= $router->url('post', ['id' => $post->getID(), 'slug' =>$post->getSlug()]) ?>" class="btn btn-primary">Voir plus</a>
