5e3533169dd03e8016d748cbdebefb14d598b740
guerroui.m@gmail.com <guerroui.m@gmail.com>
2020-12-11 14:41:56 +0100

    TP_Blog_PHP [Affichage des catégories sur le listing : 'amélioration du code dans show.php(dossier post)']

--- 44Blog(TP)/views/post/show.php
+++ 44Blog(TP)/views/post/show.php
@@ -1,46 +1,26 @@
 <?php
 
 use App\Connection;
-use App\model\Category;
-use App\Model\Post;
+use App\Table\PostTable;
+use App\Table\CategoryTable;
 
 $id= (int)$params['id'];
 $slug= (int)$params['slug'];
 
 $pdo = Connection::getPDO();
-$query = $pdo->prepare('SELECT * FROM post WHERE id = :id');
-$query->execute(['id' => $id]);
-$query->setFetchMode(PDO::FETCH_CLASS, Post::class);
-/** @var Post|false */
-$post = $query->fetch();
-
-if ($post === false) {
-    throw new Exception('Aucun article ne correspond à cet ID');
-}
+$post = (new PostTable($pdo))->find($id);
+(new CategoryTable($pdo))->hydratePosts([$post]);
 
 if($post->getSlug() !== $slug) {
     $url = $router->url('post', ['slug' => $post->getSlug(), 'id' => $id]);
     //http_response_code(301);
     //header('Location: ' . $url);
 }
-
-$query = $pdo->prepare('SELECT * FROM post_category pc WHERE pc.post_id = :id');
-
-
-$query = $pdo->prepare('
-SELECT c.id, c.slug, c.name
-FROM post_category pc
-JOIN category c ON pc.category_id = c.id
-WHERE pc.post_id = :id');
-$query->execute(['id' => $post->getId()]);
-$query->setFetchMode(PDO::FETCH_CLASS, Category::class);
-$categories = $query->fetchAll();
-
 ?>
 
 <h1><?= e($post->getName()) ?></h1>
 <p class="text-muted"><?= $post->getCreatedAt()->format('d F Y H:i') ?></p>
-<?php foreach($categories as $k => $category):
+<?php foreach($post->getCategories() as $k => $category):
     if ($k > 0):
         echo ',';
     endif;
