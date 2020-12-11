cbc1c10e0663d48d6f00a6b6d9af73d8e1d09479
guerroui.m@gmail.com <guerroui.m@gmail.com>
2020-12-11 13:48:53 +0100

    TP_Blog_PHP [Affichage des catégories sur le listing : 'création de la class PostTable']

---
+++ 44Blog(TP)/src/Table/PostTable.php
@@ -0,0 +1,43 @@
+<?php
+namespace App\Table;
+
+use PDO;
+use App\Model\Post;
+use App\model\Category;
+use App\PaginatedQuery;
+
+class PostTable {
+
+    private $pdo;
+
+    public function __construct(\PDO $pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+    public function findPaginated ()
+    {
+        $paginatedQuery = new PaginatedQuery(
+            "SELECT * FROM post ORDER BY created_at DESC",
+            "SELECT COUNT(id) FROM post",
+            $this->pdo
+        );
+        $posts = $paginatedQuery->getItems(Post::class);
+        $postsByID = [];
+        foreach ($posts as $post) {
+            $postsByID[$post->getID()] = $post;
+        }
+        $categories = $this->pdo
+            ->query('SELECT c.*, pc.post_id
+                    FROM post_category pc
+                    JOIN category c ON c.id = pc.category_id
+                    WHERE pc.post_id IN (' . implode(',', array_keys($postsByID)) . ')'
+                    )->fetchAll(PDO::FETCH_CLASS, Category::class);
+
+        foreach ($categories as $category) {
+            $postsByID[$category->getPostID()]->addCategory($category);
+        }
+        return [$posts, $paginatedQuery];
+    }
+
+}
--- 44Blog(TP)/views/post/index.php
+++ 44Blog(TP)/views/post/index.php
@@ -1,34 +1,15 @@
 <?php
-use App\URL;
 use App\Connection;
-use App\Model\Post;
-use App\Helpers\Text;
-use App\model\Category;
-use App\PaginatedQuery;
+use App\Table\PostTable;
 
 $title = 'Mon blog';
 $pdo = Connection::getPDO();
 
-$paginatedQuery = new PaginatedQuery(
-    "SELECT * FROM post ORDER BY created_at DESC",
-    "SELECT COUNT(id) FROM post"
-);
-$posts = $paginatedQuery->getItems(Post::class);
-$postsByID = [];
-foreach ($posts as $post) {
-    $postsByID[$post->getID()] = $post;
-}
-$categories = $pdo
-    ->query('SELECT c.*, pc.post_id
-             FROM post_category pc
-             JOIN category c ON c.id = pc.category_id
-             WHERE pc.post_id IN (' . implode(',', array_keys($postsByID)) . ')'
-            )->fetchAll(PDO::FETCH_CLASS, Category::class);
-$link = $router->url('home');
+$table = new PostTable($pdo);
+[$posts, $pagination] = $table->findPaginated();
 
-foreach ($categories as $category) {
-    $postsByID[$category->getPostID()]->addCategory($category);
-}
+
+$link = $router->url('home');
 ?>
 
 <h1>Mon blog</h1>
@@ -42,6 +23,6 @@ foreach ($categories as $category) {
 </div>
 
 <div class="d-flex justify-content-between my-4">
-    <?= $paginatedQuery->previousLink($link) ?>
-    <?= $paginatedQuery->nextLink($link) ?>
+    <?= $pagination->previousLink($link) ?>
+    <?= $pagination->nextLink($link) ?>
 </div>
