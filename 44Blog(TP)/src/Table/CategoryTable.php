50c7acf50e48d3afe3ea19e37116c40486c55f12
guerroui.m@gmail.com <guerroui.m@gmail.com>
2020-12-11 15:55:58 +0100

    TP_Blog_PHP [Affichage des catégories sur le listing : 'réorganisation finale avec la méthode find dans la class Table']

--- 44Blog(TP)/src/Table/CategoryTable.php
+++ 44Blog(TP)/src/Table/CategoryTable.php
@@ -3,21 +3,11 @@ namespace App\Table;
 
 use \PDO;
 use App\model\Category;
-use App\Table\Exception\NotFoundException;
 
-class CategoryTable extends Table {
+final class CategoryTable extends Table {
 
-    public function find (int $id): Category
-    {
-        $query = $this->pdo->prepare('SELECT * FROM category WHERE id = :id');
-        $query->execute(['id' => $id]);
-        $query->setFetchMode(PDO::FETCH_CLASS, Category::class);
-        $result = $query->fetch();
-        if ($result === false) {
-            throw new NotFoundException('category', $id);
-        }
-        return $result;
-    }
+    protected $table = "category";
+    protected $class = Category::class;
 
     /**
      * @param App\Model\Post[] $posts
@@ -33,7 +23,7 @@ class CategoryTable extends Table {
                     FROM post_category pc
                     JOIN category c ON c.id = pc.category_id
                     WHERE pc.post_id IN (' . implode(',', array_keys($postsByID)) . ')'
-                    )->fetchAll(PDO::FETCH_CLASS, Category::class);
+            )->fetchAll(PDO::FETCH_CLASS, $this->class);
 
         foreach ($categories as $category) {
             $postsByID[$category->getPostID()]->addCategory($category);
--- 44Blog(TP)/src/Table/PostTable.php
+++ 44Blog(TP)/src/Table/PostTable.php
@@ -4,23 +4,12 @@ namespace App\Table;
 use PDO;
 use App\Model\Post;
 use App\Table\Table;
-use App\model\Category;
 use App\PaginatedQuery;
-use App\Table\Exception\NotFoundException;
 
-class PostTable extends Table {
+final class PostTable extends Table {
 
-    public function find (int $id): Post
-    {
-        $query = $this->pdo->prepare('SELECT * FROM post WHERE id = :id');
-        $query->execute(['id' => $id]);
-        $query->setFetchMode(PDO::FETCH_CLASS, Post::class);
-        $result = $query->fetch();
-        if ($result === false) {
-            throw new NotFoundException('post', $id);
-        }
-        return $result;
-    }
+    protected $table = "post";
+    protected $class = Post::class;
 
     public function findPaginated ()
     {
