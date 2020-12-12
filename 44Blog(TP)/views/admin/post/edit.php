f81e08dfd31326c5985ebb2d54e82567213fe959
guerroui.m@gmail.com <guerroui.m@gmail.com>
2020-12-12 11:00:02 +0100

    TP_Blog_PHP [Administration du site : 'ajout du formulaire pour éditer le titre d'un article']

--- 44Blog(TP)/views/admin/post/edit.php
+++ 44Blog(TP)/views/admin/post/edit.php
@@ -1,1 +1,19 @@
-<h1>Editer l'article <?= $params['id'] ?></h1>
+<?php
+
+use App\Connection;
+use App\Table\PostTable;
+
+$pdo = Connection::getPDO();
+$postTable = new PostTable($pdo);
+$post = $postTable->find($params['id']);
+?>
+
+<h1>Editer l'article <?= e($post->getName()) ?></h1>
+
+<form action="" method="POST">
+    <div class="form-group">
+        <label for="name">Titre</label>
+        <input type="text" class="form-control" name="name" value="<?= e($post->getName()) ?>">
+    </div>
+    <button class="btn btn-primary">Modifier</button>
+</form>
