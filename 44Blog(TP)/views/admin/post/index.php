dd9270a87922f8861923b0ba3b416eed18dcfb58
guerroui.m@gmail.com <guerroui.m@gmail.com>
2020-12-12 10:34:35 +0100

    TP_Blog_PHP [Administration du site : 'ajout des pages et des boutons pour éditer et supprimer un article']

--- 44Blog(TP)/views/admin/post/index.php
+++ 44Blog(TP)/views/admin/post/index.php
@@ -11,15 +11,27 @@ $link = $router->url('admin_posts');
 
 <table class="table table-striped">
     <thead>
+        <th>#</th>
         <th>Titre</th>
         <th>Actions</th>
     </thead>
     <tbody>
         <?php foreach($posts as $post): ?>
         <tr>
-            <td><?= e($post->getName()) ?></td>
+            <td>#<?= $post->getID() ?></td>
             <td>
-
+                <a href="<?= $router->url('admin_post', ['id' => $post->getID()]) ?>">
+                <?= e($post->getName()) ?>
+                </a>
+            </td>
+            <td>
+                <a href="<?= $router->url('admin_post', ['id' => $post->getID()]) ?>" class="btn btn-primary">
+                Editer
+                </a>
+                <a href="<?= $router->url('admin_post_delete', ['id' => $post->getID()]) ?>" class="btn btn-danger"
+                onclick="return confirm('Voulez-vous vraiment effectuer cette action ?')">
+                Supprimer
+                </a>
             </td>
         </tr>
         <?php endforeach ?>
