bb6ad3f09a072716b91328b843c06ba73db405b5
guerroui.m@gmail.com <guerroui.m@gmail.com>
2020-12-14 10:15:18 +0100

    TP_Blog_PHP [Valider des données : 'ajout de la methode class abstractValidator']

--- 44Blog(TP)/src/Validators/PostValidator.php
+++ 44Blog(TP)/src/Validators/PostValidator.php
@@ -1,34 +1,18 @@
 <?php
 namespace App\Validators;
 
-use App\Validator;
 use App\Table\PostTable;
 
-class PostValidator {
-
-    private $data;
-    private $validator;
+class PostValidator extends AbstractValidator {
 
     public function __construct(array $data, PostTable $table, ?int $postID = null)
     {
-        $this->data = $data;
-        $v = new Validator($data);
-        $v->rule('required', ['name', 'slug']);
-        $v->rule('lengthBetween', ['name', 'slug'], 3, 200);
-        //$v->rule('slug', 'slug');
-        $v->rule(function ($field, $value) use ($table, $postID) {
+        parent::__construct($data);
+        $this->validator->rule('required', ['name', 'slug']);
+        $this->validator->rule('lengthBetween', ['name', 'slug'], 3, 200);
+        //$this->validator->rule('slug', 'slug');
+        $this->validator->rule(function ($field, $value) use ($table, $postID) {
             return !$table->exists($field, $value, $postID);
         }, ['slug', 'name'], 'Cette valeur est déjà utilisée');
-        $this->validator = $v;
-    }
-
-    public function validate (): bool
-    {
-        return $this->validator->validate();
-    }
-
-    public function errors (): array
-    {
-        return $this->validator->errors();
     }
 }
