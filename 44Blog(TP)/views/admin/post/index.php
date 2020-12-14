a792fe5acfb8e1d66688b06cbe0ca6989305fd1e
guerroui.m@gmail.com <guerroui.m@gmail.com>
2020-12-14 15:50:54 +0100

    TP_Blog_PHP [Création d'un article: 'ajout du bouton pour créer un article']

--- 44Blog(TP)/views/admin/post/index.php
+++ 44Blog(TP)/views/admin/post/index.php
@@ -22,7 +22,9 @@ $link = $router->url('admin_posts');
     <thead>
         <th>#</th>
         <th>Titre</th>
-        <th>Actions</th>
+        <th>
+            <a href="<?= $router->url('admin_post_new') ?>" class="btn btn-success">Créer un article</a>
+        </th>
     </thead>
     <tbody>
         <?php foreach($posts as $post): ?>
