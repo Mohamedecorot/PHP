40b07e869bb0f0fc7bf1a5293c9be36a513515c8
guerroui.m@gmail.com <guerroui.m@gmail.com>
2020-12-14 16:04:04 +0100

    TP_Blog_PHP [Gestion des catégories: 'ajout du fichier default.php en tant que template']

---
+++ 44Blog(TP)/views/admin/layouts/default.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr" class="h-100">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= isset($title) ? e($title) : 'Mon site' ?></title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+</head>
+<body class="d-flex flex-column h-100">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a href="#" class="navbar-brand">Mon site</a>
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a href="<?= $router->url('admin_posts') ?>" class="nav-link">Articles</a>
+            </li>
+        </ul>
+    </nav>
+
+    <div class="container mt-4">
+        <?= $content ?>
+    </div>
+
+    <footer class="bg-light py-4 footer mt-auto">
+        <div class="container">
+            <?php if (defined('DEBUG_TIME')): ?>
+            Page générée en <?= round(1000 * (microtime(true) - DEBUG_TIME)) ?> ms
+            <?php endif ?>
+        </div>
+    </footer>
+</body>
+</html>
